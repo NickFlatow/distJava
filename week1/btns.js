508c8ad412553b7f0335f37faa757339ca5d5c44
Nick <nick.flatow@gmail.com>
2019-02-02 16:15:56 -0600

    bootstrap row, col-lg, add cart btn event

--- week1/btns.js
+++ week1/btns.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
   $('#productList').on('click','.cart',function(){
-      alert("Jello World!");
+      //grab nearest col
+      $col = $(this).closest('.col-lg');
+
+      item = $col.find('.item').text();
+      cost = $col.find('.cost').text();
+      //grabs img name not actual img
+      img = $col.find('img').attr('src');
+      // $('#productList').html('<img src ='+ img+'>');
+
+      // .css({"color": "red", "border": "2px solid red"});
+
+      // console.log(cost.text());
   });
 
 
