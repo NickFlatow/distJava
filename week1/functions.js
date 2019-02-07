5cad45217bff0c09819c30e87be4902456158317
Nick <nick.flatow@gmail.com>
2019-02-06 18:32:57 -0600

    changes

--- week1/functions.js
+++ week1/functions.js
@@ -1,3 +1,10 @@
+window.onload = function () {
+    if (localStorage.getItem("hasCodeRunBefore") === null) {
+        let shoppingCart = [];
+        localStorage.setItem("hasCodeRunBefore", true);
+    }
+}
+
 function updateCart(cartObject){
   shoppingCart.push(cartObject);
 }
